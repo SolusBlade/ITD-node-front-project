14b8ac13327e7c3311e4fd5ac960fe4eef842952
Trehub Anatolii <tridfoto@gmail.com>
2023-06-25 18:27:52 +0300

    change routes

--- src/services/routes.js
+++ src/services/routes.js
@@ -4,13 +4,11 @@ import { Navigate } from 'react-router';
 const { selectIsLoggedIn } = require('redux/auth/authSelectors');
 
 export const PublicRoute = ({ component, redirectTo = '/home' }) => {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = true;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return !isLoggedIn ? component : <Navigate to={redirectTo} />;
 };
 
 export const PrivateRoute = ({ component, redirectTo = '/welcome' }) => {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = true;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return isLoggedIn ? component : <Navigate to={redirectTo} />;
 };
