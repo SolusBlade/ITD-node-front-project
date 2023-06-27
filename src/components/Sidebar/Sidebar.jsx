33145a4d5ea6c0a29e2146818ab0278852e695a5
Trehub Anatolii <tridfoto@gmail.com>
2023-06-28 00:58:43 +0300

    add part of mobile menu effect

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -31,7 +31,7 @@ export const Sidebar = () => {
   const [editBoardModal, setEditBoardModal] = useState(false);
   const [activeItemId, setActiveItemId] = useState(null);
   const [boardToEdit, setBoardToEdit] = useState(null);
-  console.log(isLoggedIn);
+  // console.log(isLoggedIn);
 
   useEffect(() => {
     isLoggedIn && dispatch(getAllBoards());
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,30 @@
 import { Outlet } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Header } from 'components/Header/Header';
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import s from './HomePage.module.scss';
 
 const HomePage = () => {
-  
+  const [openMobile, setOpenMobile] = useState(false);
+  const closeMobileOnBackdrop = e => {
+    if (e.target === e.currentTarget) {
+      console.log(e);
+      setOpenMobile(false);
+    }
+  };
+  const backDropClass = openMobile ? s.sideBarBackDrop : s.hidden;
+  const sideBarClass = openMobile ? s.sideBarOpen : s.sideBar;
   return (
     <>
       <Suspense fallback={null}>
         <div className={s.homeWrap}>
-          <div className={s.sideBar}>
-            <Sidebar />
+          <div
+            className={backDropClass}
+            onClick={e => closeMobileOnBackdrop(e)}
+          >
+            <div className={sideBarClass}>
+              <Sidebar />
+            </div>
           </div>
           <div className={s.screenWrap}>
             <Header />
