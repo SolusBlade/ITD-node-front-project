5b3ff1d7543a970bd15280c47827754129f396dc
Rdk001 <rmaslenchenko95@gmail.com>
2023-06-27 13:10:54 +0200

    add bg images and fixed button modal

--- src/components/Modal/ButtonModalWithIcon.jsx
+++ src/components/Modal/ButtonModalWithIcon.jsx
@@ -4,7 +4,7 @@ import s from './Modal.module.scss';
 const ButtonModalWithIcon = ({ text }) => {
   return (
     <button type="submit" className={s.buttonModal}>
-      <Icon name="icon-btn-plus" width={32} height={32} />
+      <Icon name="icon-btn-plus" width={28} height={28} />
       <p className={s.buttonText}>{text}</p>
     </button>
   );
